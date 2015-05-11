79689977d838750db276904553e10db063e88fb6
eling <cgy008@outlook.com>
2015-05-11 21:47:35 +0800

    home20150511
    家里的一套，带注释的。

--- l2website/holiday/forms.py
+++ l2website/holiday/forms.py
@@ -6,17 +6,26 @@ from django.forms.extras.widgets import SelectDateWidget
 from l2website.settings import reward_reason_choices
 from django.forms.models import modelformset_factory
 
+'''
+继承module的三个form，在提交申请的三个界面使用
+
+'''
 class RewardForm(ModelForm):
+    #定义除了继承外的其他字段
     applyname = forms.CharField()
     person = Person()
+    #设置继承的module以及其中的字段
     class Meta:
         model = Reward
         fields = ('type', 'reason', 'days_apply')
+        #定义显示的widget
         widgets = {
             'days_apply': TextInput(attrs={'type': 'number','step':'0.5', 'value':0, 'min':'0'}),
              'type': Select(choices=reward_reason_choices),
         }
-    
+    '''
+    在clean 数据时，django会调用自定义的函数进行数据正确性检查
+    '''
     def clean_applyname(self):
         try:
             self.cleaned_data['person'] = Person.objects.get(name=self.cleaned_data['applyname'])    
@@ -55,7 +64,7 @@ class ApplicationForm(ModelForm):
         }
 
     def clean_days_apply(self):
-        # 
+        # 这里访问 holidaynum的时候并没有使用cleandata的形式，不知道是调用clean函数的顺序的问题还是其他原因，会提示字典中没有此关键字
         if self.cleaned_data['days_apply'] > self.holidayNum:
             raise forms.ValidationError("您没有那么多倒休")
         if ((self.cleaned_data['days_apply'] <= 0) or (float(self.cleaned_data['days_apply']) % (0.5) != 0.0)):
@@ -74,6 +83,10 @@ class RollbackForm(ModelForm):
         if (self.cleaned_data['days_apply'] <= 0) or (float(self.cleaned_data['days_apply']) % (0.5) != 0.0):
             raise forms.ValidationError("申请最小单位0.5天")
         return self.cleaned_data['days_apply']
+
+'''
+倒休申请的formset，暂时未使用，目前添加在生成审批的界面函数中
+'''
 
 def RewardFormSet():
     return modelformset_factory(Reward, extra=0,
